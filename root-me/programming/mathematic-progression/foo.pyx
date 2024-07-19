21bb5bd4fe27019ec77c9ecb905ea867e963c84a
salmi younes <shinigamir913@gmail.com>
2024-07-19 01:31:25 +0100

    fix

--- root-me/programming/mathematic-progression/foo.pyx
+++ root-me/programming/mathematic-progression/foo.pyx
@@ -1,1 +1,13 @@
-print "Welcome to SciPy 2008!"
+from cython.cimports.libc.string import memset
+from libc.stdlib cimport malloc, free
+from libc.stdio cimport printf
+from libcpp cimport bool
+
+def calc(double a, int b, double c, int d, double e) :
+	cdef double z = e
+	for i in range(d): 
+		if b == 0:
+			z = a + z + i * c 
+		else :
+			z = a + z - i * c
+	return z
--- root-me/programming/mathematic-progression/sol.py
+++ root-me/programming/mathematic-progression/sol.py
@@ -1,5 +1,6 @@
 from typing import List
 import httpx 
+from foo import calc
 import re
 
 def make_req(url: str, client) -> str:
@@ -20,6 +21,7 @@ def get_end_limit(data: str) -> List[str]:
 	regex: str = r'<sub>\d+<\/sub>'
 	match = re.findall(regex, data)
 	return match if match else None
+
 def get_u0_number(data: str) -> str:
 	regex: str = r' \= \d+'
 	match = re.search(regex, data)
@@ -33,18 +35,19 @@ def solve(a: str, b: str, c: str, d: str) -> int:
 	return memo[-1]
 
 def send_res(result, url, client) -> str:
-	req = client.get(f'{url}{result}')
+	url = f'{url}{result}'
+	print (url)
+	req = client.get(url)
 	return req.text
 
 if __name__ == '__main__':
 	with httpx.Client() as client:
 		url: str = 'http://challenge01.root-me.org/programmation/ch1/'
 		data = make_req(url, client)
-		clean = lambda x: x.replace(']', '').replace('[', '').replace('=', '').replace(' ', '')
-		a, b, c, d = clean(get_second_expr(data)), clean(get_first_expr(data)), get_end_limit(data)[-1].replace('>', ' ').replace('<', ' ').split()[1], get_u0_number(data).split()[-1]
-		result: int = solve(a, b, c,d)
+		expr: List[str] = []
+		a, b, c, d, e = int(get_second_expr(data).split()[2]), 0 if get_first_expr(data).split()[1] == '+' else 1, int(get_first_expr(data).split()[5]), int(re.search(r'\d+', get_end_limit(data)[1]).group()), int(get_u0_number(data).split()[-1])
+		res = int(calc(a, b, c, d, e))
+		print (int(res))
 		url: str = 'http://challenge01.root-me.org/programmation/ch1/ep1_v.php?result='
-		print (result)
-		print (send_res(result, url, client))
-
+		print (send_res(res, url, client))
 
